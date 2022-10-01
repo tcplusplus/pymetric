b92fbba05fb845b33023408d70ef604f04c4c315
TomC <tcuypers@gmail.com>
2022-10-01 22:09:29 +0200

    Adding SQRT

--- src/results/Result.py
+++ src/results/Result.py
@@ -1,26 +1,38 @@
 
-from typing import Generic, Literal, TypeVar, Union, overload
+from abc import abstractmethod
+import traceback
+from typing import Callable, Generic, List, Literal, TypeVar, Union, overload
 
 B = TypeVar('B', bound=bool)
 C = TypeVar('C', bound=bool)
 T = TypeVar('T')
+T2 = TypeVar('T2')
 E = TypeVar('E', bound=str)
+E2 = TypeVar('E2', bound=str)
 
 class ResultBase(Generic[B, C]):
     def __init__(self, success: B, failure: C) -> None:
         self.is_success: B = success
         self.is_failure: C = failure
 
-class Success(Generic[T], ResultBase[Literal[True], Literal[False]]):
+class Success(Generic[T, E], ResultBase[Literal[True], Literal[False]]):
     def __init__(self, value: T) -> None:
         super().__init__(success=True, failure=False)
         self.value: T = value
 
-class Failure(Generic[E], ResultBase[Literal[False], Literal[True]]):
-    def __init__(self, error_code: E) -> None:
+    def map(self, callback: Callable[[T], T2]) -> 'Success[T2, E]':
+        return Success(callback(self.value))
+
+class Failure(Generic[T, E], ResultBase[Literal[False], Literal[True]]):
+    def __init__(self, error_code: E, message: str, stack: List[str] = traceback.format_list(traceback.extract_stack())) -> None:
         super().__init__(success=False, failure=True)
         self.error_code = error_code
+        self.message = message
+        self.stack = stack
 
-Result = Union[Success[T], Failure[E]]
+    def map(self, callback: Callable[[T], T2]) -> 'Failure[T2, E]':
+        return Failure(error_code=self.error_code, message=self.message, stack=self.stack)
+
+Result = Union[Success[T, E], Failure[T, E]]
 
 
--- src/results/Result_test.py
+++ src/results/Result_test.py
@@ -5,7 +5,7 @@ from .Result import Failure, Result, Success
 def double_if_pos (value: int) -> Result[int, Literal['negative']]:
     if value >= 0:
         return Success(value * 2)
-    return Failure('negative')
+    return Failure('negative', f'value {value} cannot be passed to double_if_pos')
 
 class TestResult:
     def test_result_success(self) -> None:
@@ -27,3 +27,25 @@ class TestResult:
             pass
         if ret.is_failure:
             assert ret.error_code == 'negative'
+
+    def test_failure_message_and_stack(self) -> None:
+        ret = double_if_pos(-2)
+        assert ret.is_failure
+        if ret.is_failure:
+            assert ret.message == 'value -2 cannot be passed to double_if_pos'
+            assert len(ret.stack) > 1
+
+    def test_map_the_value_of_a_positive_result(self) -> None:
+        ret = double_if_pos(4)
+        new_ret = ret.map(lambda v: f'val: {v}')
+        assert new_ret.is_success
+        if new_ret.is_success:
+            assert new_ret.value == 'val: 8'
+
+    def test_map_the_value_if_failure(self) -> None:
+        ret = double_if_pos(-4)
+        new_ret = ret.map(lambda v: f'val: {v}')
+        assert new_ret.is_failure
+        if new_ret.is_failure:
+            assert new_ret.error_code == 'negative'
+
